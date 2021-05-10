efedc86d277eda575e73d1bd1c420d99944becae
Alejahndro <alejahndro02@gmail.com>
2021-05-10 12:32:38 -0500

    Se inyectan las depencias atravez del modulo HttpClient

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,5 +7,5 @@ import { Injectable } from '@angular/core';
 export class ShortUrlService {
   url = 'https://api-ssl.bitly.com/v4/shorten';
   token = 'efc953ec94f85548aca0f9eea033f0214949af26';
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
