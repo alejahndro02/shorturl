73ebd8eb1e51ac566730b29e0db1c2c7279df011
Alejahndro <alejahndro02@gmail.com>
2021-05-10 12:29:40 -0500

    Se declara la variable url y la varable token donde se le asigna le valor del valor del token que nos proporciona bitly para el uso de su api

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ShortUrlService {
-
+  url = 'https://api-ssl.bitly.com/v4/shorten';
+  token = 'efc953ec94f85548aca0f9eea033f0214949af26';
   constructor() { }
 }
