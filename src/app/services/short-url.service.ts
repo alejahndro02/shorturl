9b8750cf17cb1e77f5217dfd305395df4808be87
Alejahndro <alejahndro02@gmail.com>
2021-05-10 15:32:25 -0500

    Se injectan los servicios de httpclient, tambien se importan los httpHeaders para poder usar correctamente el metodo post dentro del metod getUrlShort(

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,15 @@ import { Injectable } from '@angular/core';
 export class ShortUrlService {
   url = 'https://api-ssl.bitly.com/v4/shorten';
   token = 'efc953ec94f85548aca0f9eea033f0214949af26';
-  constructor(private http: HttpClient) { }
+  
+  constructor(private http: HttpClient) { 
+  }
+
+  getUrlShort(nombreUrl: string):Observable<any>{
+    const tokenHeader = new HttpHeaders({Authorization : 'Bearer'+ this.token})
+    const body={
+      long_url: nombreUrl
+    }
+    return this.http.post(this.url, body, {headers: tokenHeader})
+  }
 }
