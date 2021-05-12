9569d585cc977139d5ce3704616d3a542ab29db9
Alejahndro <alejahndro02@gmail.com>
2021-05-11 19:53:47 -0500

    Se agrego el token y el headers

--- src/app/services/short.interceptor.ts
+++ src/app/services/short.interceptor.ts
@@ -3,9 +3,11 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ShortInterceptor implements HttpInterceptor {
@@ -13,6 +15,14 @@ export class ShortInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = 'efc953ec94f85548aca0f9eea033f0214949af26';
+    request = request.clone({setHeaders: {Authorization : 'Bearer ' + token}});
     return next.handle(request);
+    //si se requiere manipular el error desde el interceptor
+    // return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
+    //   console.log(error);
+    //   return throwError
+    // }));
+
   }
 }
